782c492b666ef28272bd3fdfff1faed21cd9ca45
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2024-02-12 18:26:23 +0200

    Add services

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const sliderWrapper = document.querySelector('.sliderWrapper');
-
 const menuItems = document.querySelectorAll('.menuItem');
+
+/*changing slides*/
 menuItems.forEach((item, index)=>{
   item.addEventListener('click', ()=>{
     sliderWrapper.style.transform = `translateX(${-100 * index}vw)`;
